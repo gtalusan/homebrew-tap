807590b204ca12c75f26745e359f38eaf8628dcb
Pyrolistical <pyrogx1133@gmail.com>
2013-01-19 16:33:05 -0800

    added screen arrangement formula

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ tap
 ===
 
 Homebrew tap for formulas created by Concept Not Found
+
+Add this tap: `brew tap homebrew/dupes`
+
+Available formulas:
+ * `brew install screenarrangement`
+
---
+++ screenarrangement.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Screenarrangement < Formula
+  homepage 'https://github.com/concept-not-found/screen-arrangement'
+  url 'https://github.com/concept-not-found/screen-arrangement/raw/release/screenarrangement-1.tar.gz'
+  sha1 '3f1a3e4ca4524909ab56ec3ecddc118bbf31e696'
+
+  def install
+    system "make release"
+	system "mkdir #{prefix}/bin"
+	system "cp build/Release/screenarrangement #{prefix}/bin"
+  end
+end
