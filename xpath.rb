cc90711d4ebbd44d9abd7394b3a00505a6d273f1
George Talusan <george.talusan@gmail.com>
2017-08-14 15:22:21 -0400

    sha256

--- xpath.rb
+++ xpath.rb
@@ -4,7 +4,7 @@ class Xpath < Formula
   homepage 'http://packages.debian.org/squeeze/libxml-xpath-perl'
   url 'http://mirrors.us.kernel.org/ubuntu//pool/universe/libx/libxml-xpath-perl/libxml-xpath-perl_1.13-7_all.deb'
   version '1.13-7'
-  sha1 '5d70f8054a7596727d05918b01b3617effe47937'
+  sha256 'edd92c1d09e17faf89396ea06309ae75836065d1da3f7f8c4bf74241f7a9937e'
 
   def install
     system "tar xf data.tar.gz"
